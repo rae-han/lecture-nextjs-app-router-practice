a28832f4244c0bafed71fe5f7f1aaab059965fe6
raehan <raehan900809@gmail.com>
2024-10-04 18:08:11 +0900

    feature: form, input 테스트 할 페이지와 컴포넌트 생성

---
+++ src/app/form/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Form from '@components/Form';
+import Input from '@components/Input';
+
+const Page = () => {
+  return (
+    <Form>
+      <Input />
+    </Form>
+  );
+};
+
+export default Page;
